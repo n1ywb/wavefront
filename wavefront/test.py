36e5cf573d4660dc804afb2e0394f981a1b585db
Jeff Laughlin <jeff@jefflaughlinconsulting.com>
2013-09-23 13:35:40 -0400

    refactored test

--- wavefront/test.py
+++ wavefront/test.py
@@ -12,49 +12,51 @@ from antelope import brttpkt
 
 from wavefront.controller import Orb
 
+class Dummy(object): pass
+
+def makepkt(time):
+    pkt = Dummy()
+    srcname = Dummy()
+    srcname.net = 'pktnet'
+    srcname.sta = 'pktsta'
+    srcname.chan = 'pktchan'
+    srcname.loc = 'pktloc'
+    srcname.suffix = 'pktsuffix'
+    srcname.subcode = 'pktsubcode'
+    pkt.srcname = srcname
+    channel = Dummy()
+    channel.data = [1,2,3,4,5]
+    channel.samprate = 1
+    channel.chan = 'chanchan'
+    channel.loc = 'chanloc'
+    channel.net = 'channet'
+    channel.sta = 'chansta'
+    channel.time = time
+    pkt.channels = [channel]
+    return pkt
+
+def make_mock_proc_orb(maxcnt, app, orb):
+    count = [0]
+
+    def _mock_proc(self, *args, **kwargs):
+        Orb._process(self, *args, **kwargs)
+        count[0] += 1
+        if count[0] >= maxcnt:
+            app.kill()
+
+    orb._process = MethodType(_mock_proc, orb)
+
+
 def test_app():
-    class Dummy(object): pass
-
-    count = [0,0]
-
-    def makepkt(time):
-        pkt = Dummy()
-        srcname = Dummy()
-        srcname.net = 'pktnet'
-        srcname.sta = 'pktsta'
-        srcname.chan = 'pktchan'
-        srcname.loc = 'pktloc'
-        srcname.suffix = 'pktsuffix'
-        srcname.subcode = 'pktsubcode'
-        pkt.srcname = srcname
-        channel = Dummy()
-        channel.data = [1,2,3,4,5]
-        channel.samprate = 1
-        channel.chan = 'chanchan'
-        channel.loc = 'chanloc'
-        channel.net = 'channet'
-        channel.sta = 'chansta'
-        channel.time = time
-        pkt.channels = [channel]
-        return pkt
-
     for n in xrange(2):
         brttpkt.get_rvals.appendleft((n, 'foobar', n*5, makepkt(n*5)))
 
     from wavefront.controller import App
 
     app = App()
-
-    def _mock_proc(self, *args, **kwargs):
-        Orb._process(self, *args, **kwargs)
-        count[0] += 1
-        if count[0] == 2:
-            app.kill()
-
     orb = app.add_orb('anfprelim')
-    orb._process = MethodType(_mock_proc, orb)
-
     orb.add_binner('channet_chansta_chanchan_chanloc', twin=10.0, tbin=5.0)
+    make_mock_proc_orb(2, app, orb)
     app.start()
     gevent.wait()
 
