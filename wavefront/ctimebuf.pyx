92b1ec068c184ea53854ee7cf16065f9b9dbcfc8
Jeff Laughlin <jeff@jefflaughlinconsulting.com>
2013-09-26 08:17:07 +0000

    more cython optimizations

--- wavefront/ctimebuf.pyx
+++ wavefront/ctimebuf.pyx
@@ -1,17 +1,36 @@
 # cython: profile=True
 
+import logging
+
+log = logging.getLogger(__name__)
+
+import cython
 
 
-from numbers import Number
+cdef class MagicList:
+    cdef list _list
 
-class MagicList(list):
-    def __getitem__(self, n):
-        return super(MagicList, self).__getitem__(n % len(self))
+    def __init__(self, iterable=None):
+        self._list = list()
+        if iterable is not None:
+            self._list = list(iterable)
 
-    def __setitem__(self, n, v):
-        super(MagicList, self).__setitem__(n % len(self), v)
+    @cython.cdivision(True)
+    def __getitem__(self, int n):
+        return self._list[n % len(self)]
+
+    @cython.cdivision(True)
+    def __setitem__(self, int n, v):
+        self._list[n % len(self)] = v
+
+    def __len__(self):
+        return len(self._list)
+        
 
 cdef class TimeUtil:
+#    cdef public double element_time
+
+    @cython.cdivision(True)
     cpdef int index(self, double timestamp):
         return int(timestamp / self.element_time)
 
@@ -21,7 +40,7 @@ cdef class TimeUtil:
     cpdef double floor(self, double timestamp):
         return self.timestamp(self.index(timestamp))
 
-class TimeBuffer(TimeUtil):
+cdef class TimeBuffer(TimeUtil):
     """Associative circular time series buffer of timestamp/value pairs.
 
     From tail to head timestamps are contiguous and monotonically increasing.
@@ -32,24 +51,28 @@ class TimeBuffer(TimeUtil):
 
     Supports extended slicing, but step must be an integer.
     """
+#    cdef public double element_time
+    cdef public int head_num
+    cdef public object item_factory
+    cdef public int size
+    cdef public object _buffer
+ 
+    filler = None
 
-    def __init__(self, size, head_time, element_time, item_factory=lambda: None):
+    def __init__(self, size, head_time, element_time):
+        assert element_time != 0.0
         self.element_time = element_time
         self.head_num = int(head_time / element_time)
-        self.item_factory = item_factory
         self.size = size
-        self._buffer = MagicList([item_factory() for n in xrange(size)])
+        self._buffer = MagicList([self.filler,] * size)
 
-    @property
-    def tail_num(self):
+    cpdef int tail_num(self):
         return self.head_num - self.size
 
-    @property
-    def tail_time(self):
-        return self.tail_num * self.element_time
+    cpdef double tail_time(self):
+        return self.tail_num() * self.element_time
 
-    @property
-    def head_time(self):
+    cpdef double head_time(self):
         return self.head_num * self.element_time
 
     def __repr__(self):
@@ -58,14 +81,14 @@ class TimeBuffer(TimeUtil):
     def __str__(self):
         return str(dict(self.iteritems()))
 
-    def append(self, item):
+    cpdef append(self, item):
         """Append exactly one item to the buffer. Item timestamp must be
         equal to head_time.
         """
         self._buffer[self.head_num] = item
         self.head_num += 1
 
-    def update(self, k, v):
+    cpdef update(self, double k, v):
         """Update
 
         Values are put into the buffer. If a value is the newest, it's appended
@@ -76,7 +99,7 @@ class TimeBuffer(TimeUtil):
             if self.index(k) > self.head_num + self.size:
                 self.head_num = self.index(k) - self.size
             for n in xrange(self.head_num, self.index(k)):
-                self.append(self.item_factory())
+                self.append(self.filler)
             self.append(v)
         else:
             try:
@@ -84,33 +107,36 @@ class TimeBuffer(TimeUtil):
             except IndexError:
                 pass
 
-    def __getitem__(self, n):
-        if n not in self:
+    def __getitem__(self, double n):
+        if not self.has_key(n):
             raise IndexError
         return self._buffer[self.index(n)]
 
-    def __setitem__(self, n, v):
-        if n not in self:
+    def __setitem__(self, double n, v):
+        if self.has_key(n) :
             raise IndexError
         self._buffer[self.index(n)] = v
         
-    def get(self, key, default=None):
+    cpdef get(self, double key, default=None):
         try:
             return self[key]
         except IndexError:
             return default
 
-    def has_key(self, timestamp):
+    cpdef has_key(self, double timestamp):
         """True if timestamp is within the buffer boundaries."""
-        return (True if timestamp >= self.tail_time and
-                    timestamp < self.head_time else False)
+        return (True if timestamp >= self.tail_time() and
+                    timestamp < self.head_time() else False)
 
-    def __contains__(self, timestamp):
+    def __contains__(self, double timestamp):
         """True if timestamp is within the buffer boundaries."""
-        return self.has_key(timestamp)
+        return (True if timestamp >= self.tail_time() and
+                    timestamp < self.head_time() else False)
+
 
     def iteritems(self, start=None, stop=None):
-        start = self.tail_num if start is None else self.index(start)
+        cdef int n
+        start = self.tail_num() if start is None else self.index(start)
         stop = self.head_num if stop is None else self.index(stop)
         for n in xrange(start, stop):
             try:
@@ -120,7 +146,8 @@ class TimeBuffer(TimeUtil):
             yield k,v
 
     def itervalues(self, start=None, stop=None):
-        start = self.tail_num if start is None else self.index(start)
+        cdef int n
+        start = self.tail_num() if start is None else self.index(start)
         stop = self.head_num if stop is None else self.index(stop)
         for n in xrange(start, stop):
             try:
@@ -130,5 +157,5 @@ class TimeBuffer(TimeUtil):
                 pass
 
     def __iter__(self):
-        return (n * self.element_time for n in xrange(self.tail_num, self.head_num))
+        return (n * self.element_time for n in xrange(self.tail_num(), self.head_num))
 
